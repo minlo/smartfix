4e34f2a160e6eaaa8cbfe01ca28b463227ef2372
zhaoyizhaoyi <zhaoyizhaoyi@sjtu.edu.cn>
2018-01-07 22:14:20 +0800

    ready for test

--- train_test/train_test_utils.py
+++ train_test/train_test_utils.py
@@ -213,8 +213,8 @@ def search_regression_ml(data_train, save_k_best, look_ahead_day, split_date, va
         "reducer__n_components": [10]
     }
     model_dict = {
-        # "random_forest": RandomForestRegressor(n_estimators=1000, n_jobs=-1, random_state=1234),
-        # "xgboost": XGBRegressor(),
+        "random_forest": RandomForestRegressor(n_estimators=1000, n_jobs=-1, random_state=1234),
+        "xgboost": XGBRegressor(),
         "lasso": Lasso(alpha=0.01, random_state=1234),
     }
     model_param_grid_dict = {
