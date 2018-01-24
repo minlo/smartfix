461305b9ceb44d8c464d7f9401d80f1197092d97
zhaoyi <zhaoyizhaoyi@sjtu.edu.cn>
2018-01-24 16:48:09 +0800

    test for all models

--- config/warning_model_config.py
+++ config/warning_model_config.py
@@ -21,33 +21,33 @@ reducer_param_grid = {
 }
 model_dict = {
     "random_forest": RandomForestClassifier(n_estimators=1000, n_jobs=-1, random_state=1234),
-    # "xgboost": XGBClassifier(),
+    "xgboost": XGBClassifier(),
     "logistic_regression": LogisticRegression(penalty="l1"),
-    # "svm": SVC(probability=True)
+    "svm": SVC(probability=True)
 }
 model_param_grid_dict = {
     "random_forest": {
         "model__n_estimators": [1000]
     },
-    # "xgboost": {
-    #     "model__max_depth": range(2, 12, 2),
-    #     "model__min_child_weight": range(2, 10, 2),
-    #     "model__subsample": [i / 10.0 for i in range(6, 10)],
-    #     "model__colsample_bytree": [0.1, 0.2, 0.3, 0.4, 0.5],
-    #     "model__learning_rate": [0.01, 0.1]
-    # },
-    # "svm": {
-    #     "model__C": [0.01, 0.1, 1, 10, 100, 1000],
-    #     "model__kernel": ["linear", "poly", "rbf", "sigmoid"],
-    #     "model__epsilon": [x / 100 for x in range(10, 50, 5)],
-    #     "model_gamma": ["auto"]
-    # },
+    "xgboost": {
+        "model__max_depth": range(2, 12, 2),
+        "model__min_child_weight": range(2, 10, 2),
+        "model__subsample": [i / 10.0 for i in range(6, 10)],
+        "model__colsample_bytree": [0.1, 0.2, 0.3, 0.4, 0.5],
+        "model__learning_rate": [0.01, 0.1]
+    },
+    "svm": {
+        "model__C": [0.01, 0.1, 1, 10, 100, 1000],
+        "model__kernel": ["linear", "poly", "rbf", "sigmoid"],
+        "model__epsilon": [x / 100 for x in range(10, 50, 5)],
+        "model_gamma": ["auto"]
+    },
     "logistic_regression": None
 }
 model_pipeline_mode_dict = {
     "random_forest": "grid",
-    # "xgboost": "random",
+    "xgboost": "random",
     "logistic_regression": "single",
-    # "svm": "random"
+    "svm": "random"
 }
 
